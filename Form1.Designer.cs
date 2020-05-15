ca161274ad25aa5821e184225766c75c9d347219
Aditi <Aditipoudel59@gmail.com>
2020-05-15 17:44:39 +0545

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace catchmeifyoucan
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbanimal = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbanimal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lblScore);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-34, -57);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1987, 104);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(143, 64);
+            this.lblScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(19, 20);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "0";
+            this.lblScore.Click += new System.EventHandler(this.lblScore_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Score:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, -3);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pbanimal
+            // 
+            this.pbanimal.BackColor = System.Drawing.Color.Transparent;
+            this.pbanimal.Image = ((System.Drawing.Image)(resources.GetObject("pbanimal.Image")));
+            this.pbanimal.Location = new System.Drawing.Point(17, 69);
+            this.pbanimal.Name = "pbanimal";
+            this.pbanimal.Size = new System.Drawing.Size(70, 70);
+            this.pbanimal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbanimal.TabIndex = 2;
+            this.pbanimal.TabStop = false;
+            this.pbanimal.Click += new System.EventHandler(this.pbanimal_Click);
+            this.pbanimal.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbanimal_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1370, 699);
+            this.Controls.Add(this.pbanimal);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Touch MeToScore!";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.form1_MouseClick);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbanimal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbanimal;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace catchmeifyoucan
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+        int score = 0;
+        Random r = new Random();
+        int x, y;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x = r.Next(95, 1100);
+            y = r.Next(150, 600);
+            pbanimal.Location = new Point(x, y);
+           
+        }
+        int speed = 800;
+        bool play = true;
+
+        private void form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+  
+
+        private void pbanimal_Click(object sender, EventArgs e)
+        {
+            if (play==true){
+            score++;
+            lblScore.Text = score.ToString();
+            timer1.Interval = speed + 1;
+            SoundPlayer sp = new SoundPlayer(@"C:\Users\aditi\Desktop\Project\catchmeifyoucan\shotgun-spas_12-RA_The_Sun_God-503834910.wav");
+            sp.Play();
+          
+            }
+            if (score == 5)
+            {
+                timer1.Stop();
+                SoundPlayer sp = new SoundPlayer(@"C:\Users\aditi\Desktop\Project\catchmeifyoucan\shotgun-spas_12-RA_The_Sun_God-503834910.wav");
+                sp.Stop();
+                play = false;
+                MessageBox.Show("Oh  no you killed me! YOU WON");
+               
+
+               
+            
+            }      
+        }
+
+       private void lblScore_Click(object sender, EventArgs e)
+        {
+
+       }
+    private void pbanimal_MouseClick(object sender, MouseEventArgs e)
+      {
+    
+        }
+
+    private void panel1_Paint(object sender, PaintEventArgs e)
+    {
+
+    }
+
+        
+     
+       
+    }
+}
